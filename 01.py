6fbbfeacc73c54a9ccdbd39aa0e4a112d62c3bb0
Juraj Pangrác <xpangra1@fi.muni.cz>
2025-12-02 18:50:27 +0100

    Day 1 Task 2

--- 01.py
+++ 01.py
@@ -1,6 +1,9 @@
 FILENAME: str = "01_input.txt"
 START: int = 50
 
+LEFT = -1
+RIGHT = 1
+
 def task1(FILENAME, START):
     position: int = START
     zero_count: int = 0
@@ -13,22 +16,44 @@ def task1(FILENAME, START):
             rotation: tuple[int, int] = parse(line)
             direction, clicks = rotation
 
-            position += (direction * clicks) % 100
+            position += (direction * clicks)
             position %= 100
 
             if position == 0:
                 zero_count += 1
 
     return zero_count
+
+
+def task2(FILENAME, START):
+    position: int = START
+    zero_count: int = 0
+
+    with open(FILENAME) as f:
+        lines: list[str] = f.readlines()
+        for line in lines:
+            line = line.strip("\n")
+
+            rotation: tuple[int, int] = parse(line)
+            direction, clicks = rotation
+
+            zero_count += clicks // 100
+            clicks %= 100
+            
+            prev = position
+            position += direction * clicks
+            if position > 99 or (position <= 0 and prev != 0):
+                zero_count += 1
+
+            position %= 100
+
+    return zero_count
             
 
 def parse(line: str):
-    r1: int
-    r2: int
-
-    r1 = 1 if line[0] == "R" else -1
-    r2 = int(line[1:])
-
+    r1: int = RIGHT if line[0] == "R" else LEFT
+    r2: int = int(line[1:])
     return (r1, r2)
 
 print(f"The code to day 1 part 1 is: {task1(FILENAME, START)}")
+print(f"The code to day 1 part 2 is: {task2(FILENAME, START)}")
