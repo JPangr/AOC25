3b2f5a69f9a164fe6b5e9e5211b3fb38afebc72c
Juraj Pangrác <xpangra1@fi.muni.cz>
2025-12-04 19:53:12 +0100

    Day 2 task 2

--- 02.py
+++ 02.py
@@ -12,13 +12,38 @@ def task1(FILENAME):
         # not a very clever solution; there may be a better way of
         # doing this than brute force, but I haven't found it yet
         for id in range(int(ids[0]), int(ids[1])+1):
-            if not is_valid(str(id)):
+            if not is_valid_1(str(id)):
                 result += int(id)
     
     return result
 
 
-def is_valid(id: str):
+def task2(FILENAME):
+    result: int = 0
+
+    with open(FILENAME) as f:
+        line: str = f.readline()
+
+    seqs: list[str] = line.split(",")
+    for seq in seqs:
+        ids: list[str] = seq.split("-")
+        for id in range(int(ids[0]), int(ids[1])+1):
+            # ditto as before, only even more grossly inefficient
+            if not is_valid_2(str(id)):
+                result += int(id)
+    
+    return result
+
+
+def is_valid_1(id: str):
     return id[:len(id)//2] != id[len(id)//2:]
 
+
+def is_valid_2(id: str):
+    for i in range(1, len(id)):
+        if id == id[:i] * (len(id) // i):
+            return False
+    return True
+
 print(f"The code to day 2 part 1 is: {task1(FILENAME)}")
+print(f"The code to day 2 part 2 is: {task2(FILENAME)}")
