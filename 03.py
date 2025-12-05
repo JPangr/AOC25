09568372d8a949dcebcb639a59af136829549bba
Juraj Pangrác <xpangra1@fi.muni.cz>
2025-12-05 01:49:53 +0100

    Day 3 task 2

--- 03.py
+++ 03.py
@@ -21,4 +21,39 @@ def task1(FILENAME):
 
     return result
 
+
+def task2(FILENAME):
+    result: int = 0
+    with open(FILENAME) as f:
+        lines: list[str] = f.readlines()
+    
+    for line in lines:
+        line = line.strip("\n")
+        
+        positions: list[int] = 12 * [0]
+
+        # looks a little convoluted, but is actually pretty simple
+        # we find the position P0 with the greatest value in the range [0..n]
+        # then we look for the position P1, in the range [P0+1..n+1]
+        # then P2, in the range [P1..n+2]
+        # etc
+        # where n is length of our input minus the desired length of our output + 1
+        prev = -1
+        for i in range(12):
+            positions[i] = prev+1
+            for k in range(prev+1, len(line) - (11-i)):
+                if ord(line[k]) > ord(line[positions[i]]):
+                    positions[i] = k
+            prev = positions[i]
+
+        thing = ""
+        for i, pos in enumerate(positions):
+            thing += line[pos]
+            result += int(line[pos]) * (10 ** (11-i))
+
+        assert(True)
+
+    return result
+
 print(f"The code to day 3 part 1 is: {task1(FILENAME)}")
+print(f"The code to day 3 part 2 is: {task2(FILENAME)}")
