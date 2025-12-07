72a4b416ff0c5b0552175ea5831021e0b3b663e3
Juraj Pangrác <xpangra1@fi.muni.cz>
2025-12-07 23:23:09 +0100

    Day 4 task 2

--- 04.py
+++ 04.py
@@ -36,4 +36,28 @@ def is_accessible(x: int, y: int, grid: list[str]):
     return count < 4
 
 
+def task2(FILENAME):
+    with open(FILENAME) as f:
+        grid: list[str] = f.readlines()
+
+
+    ln: int = len(grid[0]) # assuming all lines are the same length
+    count: int = 0
+
+    changes: int = 1 # initializing at a non0 value, since pythond doesn't have a do-while
+    while changes != 0:
+        changes = 0
+        for y in range(0, len(grid)):
+            for x in range(0, ln):
+                if grid[y][x] != '@':
+                    continue
+                if is_accessible(x, y, grid):
+                    count += 1
+                    grid[y] = grid[y][:x] + "." + grid[y][x+1:]
+                    changes += 1
+    
+    return count
+
+
 print(f"The code to day 4 part 1 is: {task1(FILENAME)}")
+print(f"The code to day 4 part 2 is: {task2(FILENAME)}")
